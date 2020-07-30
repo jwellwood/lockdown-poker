ab51becd318601c7e662b812fda115e12c8805f0
jwellwood <joewellwood@gmail.com>
2020-07-31 00:10:06 +0200

    fix sorting algorithm

--- src/shared/hooks/usePlayerStatsArray.jsx
+++ src/shared/hooks/usePlayerStatsArray.jsx
@@ -1,31 +1,39 @@
-export const usePlayerStatsArray = (players, games) => {
-  const playersWithGames = players.reduce((arr, player) => {
-    const filteredGames = games.filter((game) =>
-      game.participants.map((pt) => pt.name).includes(player.id)
-    );
+import { useMemo } from 'react';
 
-    const playerGameStats = filteredGames.map((game) =>
-      game.participants.filter((pt) => pt.name === player.id)
-    );
-    arr.push({
+export const usePlayerStatsArray = (players, games) =>
+  useMemo(() => {
+    const playersWithGames = players.reduce((arr, player) => {
+      const filteredGames = games.filter((game) =>
+        game.participants.map((pt) => pt.name).includes(player.id)
+      );
+
+      const playerGameStats = filteredGames.map((game) =>
+        game.participants.filter((pt) => pt.name === player.id)
+      );
+      arr.push({
+        ...player,
+        games: filteredGames,
+        stats: playerGameStats.flat(), // TODO check browser support for this
+      });
+      return arr;
+    }, []);
+
+    const rankingByAveragePosition = playersWithGames.map((player) => ({
       ...player,
-      games: filteredGames,
-      stats: playerGameStats.flat(), // TODO check browser support for this
+      averagePosition: player.stats.length
+        ? player.stats
+            .map((stat) => stat.finalPosition)
+            .reduce((a, b) => +a + +b, 0) / player.games.length
+        : null,
+    }));
+
+    const sorted = [...rankingByAveragePosition].sort((a, b) => {
+      return (
+        (a.averagePosition === null) - (b.averagePosition === null) ||
+        -(b.averagePosition > a.averagePosition) ||
+        +(b.averagePosition < a.averagePosition)
+      );
     });
-    return arr;
-  }, []);
 
-  const rankingByAveragePosition = playersWithGames.map((player) => ({
-    ...player,
-    averagePosition:
-      player.stats
-        .map((stat) => stat.finalPosition)
-        .reduce((a, b) => +a + +b, 0) / player.games.length,
-  }));
-
-  const sortedPlayers = rankingByAveragePosition.sort(
-    (a, b) => a.averagePosition - b.averagePosition
-  );
-
-  return { playersWithGames, sortedPlayers };
-};
+    return { playersWithGames, sorted };
+  }, [players, games]);
